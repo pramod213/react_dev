2fe5642d1173bf97bd80b6d925ba71ff8b829961
Pramod Kumar <pramodkumar93344@gmail.com>
2025-10-28 02:50:48 +0530

    01-folder done

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    
+    <>
+      <div id='papa'>
+        <h1 id='child1'>Hello</h1>
+        <h2 id='child2'>Ritik</h2>
+        <h3 id='child3'>Kaise ho yaar</h3>
+      </div>
+
+      <div id="chacha">
+        <h1>Tejas</h1>
+        <h2>Iksha</h2>
+
+      </div>
+    </>
+  )
+}
+
+export default App
